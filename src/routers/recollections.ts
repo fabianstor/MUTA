7ca13ded6036706773b767041afc0312452d3cc5
Fabian Garcia <fabigg2001@hotmail.com>
2023-08-19 21:52:03 -0500

    jwt para recolecciones

--- src/routers/recollections.ts
+++ src/routers/recollections.ts
@@ -1,12 +1,13 @@
 import { Router } from "express"
 import * as collectionController from "../controllers/collections"
+import { verifyToken } from "../controllers/permissions"
 
 const colecctionRoutes = Router()
 
-colecctionRoutes.get('/', collectionController.getCollections)
-colecctionRoutes.get('/:id', collectionController.getCollection)
-colecctionRoutes.post('/', collectionController.createCollection)
-colecctionRoutes.put('/:id', collectionController.updateCollection)
-colecctionRoutes.delete('/:id', collectionController.deleteCollection)
+colecctionRoutes.get('/', verifyToken, collectionController.getCollections)
+colecctionRoutes.get('/:id', verifyToken, collectionController.getCollection)
+colecctionRoutes.post('/', verifyToken, collectionController.createCollection)
+colecctionRoutes.put('/:id', verifyToken, collectionController.updateCollection)
+colecctionRoutes.delete('/:id', verifyToken, collectionController.deleteCollection)
 
 export default colecctionRoutes
